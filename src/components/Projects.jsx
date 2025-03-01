97b61812bfd19a3798fc212a7fb0d5a06b8cdc41
TanayMoriarty <145890243+TanayMoriarty@users.noreply.github.com>
2025-03-01 19:09:45 +0530

    first commit

---
+++ src/components/Projects.jsx
@@ -0,0 +1,113 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/Hos-man.png";
+import projImg2 from "../assets/img/ScanX.png";
+import projImg3 from "../assets/img/RideX.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>"My React project portfolio showcases a range of applications. ScanX, a deepfake detector, leverages </p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content>
+                    <Tab.Pane eventKey="first">
+                      <p>"This hospital management web app streamlines operations with an intuitive interface. It efficiently manages patient records and scheduling, enhancing workflow. Robust security ensures data confidentiality. A comprehensive solution for modern healthcare management, improving overall efficiency and patient care."
+                      </p>
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      <p>"RideX delivers a seamless, real-time cab booking experience. Its intuitive interface allows for quick ride requests and accurate tracking. The system optimizes routes and manages bookings efficiently, enhancing user convenience. Real-time updates and secure payment options provide a reliable and user-friendly transport solution."
+                      </p>
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>"ScanX is a cutting-edge web app for deepfake detection. It employs advanced algorithms to analyze images and videos, identifying manipulated content with accuracy. The user-friendly interface simplifies the process, making it accessible for verifying media authenticity. ScanX provides a crucial tool in combating misinformation."
+                      </p>
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
